7f65f6f64b444ccb89af7ba32ab09975665efe18
Marlon Reghert <mras@cin.ufpe.br>
2015-07-05 17:49:31 -0300

    Slide11

---
+++ PLCThreadsJava/src/Slide11/Exercicio1.java
@@ -0,0 +1,33 @@
+package Slide11;
+import java.util.ArrayList;
+
+class Contador implements Runnable{
+	int l, r; 
+	public Contador(int _l, int _r){ 
+		l=_l; r=_r;
+		this.run();
+	}
+	public void run() {
+		for (int i = l; i <= r; i++) {
+			System.out.println(i);
+		}
+	}
+	@Override
+	public String toString() {
+		return "Contador [l=" + l + ", r=" + r + "]";
+	}
+	
+}
+
+public class Exercicio1 {
+	public static void main(String[] args) {
+		int N=2;
+		int SLICE = (2*1000000)/N;
+		int L = 0;
+		ArrayList<Contador> threads = new ArrayList<>() ;
+		for(int i =0 ; i < N; i++ ) {
+			threads.add(new Contador(L, (i==N-1) ? 2*1000000 : L+SLICE  ));			
+			L+=SLICE+1;
+		}
+	}
+}
